246c59971cac4143587cf77faabe208bfc21197b
duke <chenjunhao100@gmail.com>
2023-03-17 21:22:24 +0800

    ADD: Lexer built.

---
+++ src/stone/StoneException.java
@@ -0,0 +1,14 @@
+package stone;
+
+/**
+ * @author Duke
+ */
+public class StoneException extends RuntimeException {
+    public StoneException(String m) {
+        super(m);
+    }
+
+//    public StoneException(String m, ASTree t) {
+//        super(m + " " + t.location());
+//    }
+}
